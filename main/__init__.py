3fa7c64008923714bbc272a1595504942b1042a1
muhammad <mnasreldin2@gmail.com>
2022-03-14 15:18:49 +0200

    add gunicron

--- main/__init__.py
+++ main/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_mail import Mail
-from main.config import Config
+from config import Config
 
 
 db = SQLAlchemy()
